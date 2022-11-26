69c8730415351fb8532709bbd638dab77434acd7
al-kuznetsov <alkuznetsov@aol.com>
2022-11-26 20:38:38 +0500

    Add simple DateTime demo

---
+++ src/main/java/com/aol/alkuznetsov/datetime/DateTimeDemoUtils.java
@@ -0,0 +1,9 @@
+package com.aol.alkuznetsov.datetime;
+
+public class DateTimeDemoUtils {
+  
+  public static long getCurrentTimeInMilliseconds() {
+    return System.currentTimeMillis();
+  }
+
+}
---
+++ src/main/java/com/aol/alkuznetsov/datetime/DemoDriver.java
@@ -0,0 +1,9 @@
+package com.aol.alkuznetsov.datetime;
+
+public class DemoDriver {
+
+  public static void main(String[] args) {
+    
+    System.out.println(DateTimeDemoUtils.getCurrentTimeInMilliseconds());
+  }
+}
